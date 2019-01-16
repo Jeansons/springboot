5c94d6a551db0e97f91aa61fcb35b22d5916e371
ycp <ycp@ycp-PC>
2019-01-16 13:53:17 +0800

    scope

---
+++ src/main/java/spring4/ch2/scope/DemoSingletonService.java
@@ -0,0 +1,9 @@
+package spring4.ch2.scope;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoSingletonService {
+	
+
+}
---
+++ src/main/java/spring4/ch2/scope/ScopeConfig.java
@@ -0,0 +1,10 @@
+package spring4.ch2.scope;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("spring4.ch2.scope")
+public class ScopeConfig {
+
+}
