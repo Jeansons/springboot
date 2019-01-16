d255bdb521c1866eacbf3541f900fc58317d895c
ycp <ycp@ycp-PC>
2019-01-16 16:44:58 +0800

    profile

---
+++ src/main/java/spring4/ch2/profile/DemoBean.java
@@ -0,0 +1,17 @@
+package spring4.ch2.profile;
+
+public class DemoBean {
+	private String content;
+	public DemoBean(String content) {
+		super();
+		this.content=content;
+	}
+	public String getContent() {
+		return content;
+		
+	}
+	public void setContent(String content) {
+		this.content=content;
+		
+	}
+}
