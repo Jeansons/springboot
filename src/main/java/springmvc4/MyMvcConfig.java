aff413514cb747c14036af36c1f20674d646bc08
ycp <ycp@ycp-PC>
2019-01-21 17:23:40 +0800

    addViewControllers(ViewControllerRegistry的用法)页面转向

--- src/main/java/springmvc4/MyMvcConfig.java
+++ src/main/java/springmvc4/MyMvcConfig.java
@@ -3,23 +3,29 @@ package springmvc4;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 
 @Configuration
 @EnableWebMvc  //@EnableWebMvc 开启springmvc支持，若无，重写webmvcConfigurerAdaper方法无效。
+@EnableScheduling
 @ComponentScan("springmvc4")
 public class MyMvcConfig implements WebMvcConfigurer{//官方推荐 实现WebMvcConfigurer
+//public class MyMvcConfig extends WebMvcConfigurerAdapter{
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver=
 				new InternalResourceViewResolver();
 		viewResolver.setPrefix("/WEB-INF/classes/views/");
+		//viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
@@ -29,7 +35,7 @@ public class MyMvcConfig implements WebMvcConfigurer{//官方推荐 实现WebMvc
 		registry.addResourceLocations("/assets/**")
 		.addResourceLocations("classpath:/assets/");
 	}
-	
+
 	@Bean  // 配置拦截器的Bean
 	public DemoInterceptor demoInterceptor() {
 		return new DemoInterceptor();
@@ -38,6 +44,11 @@ public class MyMvcConfig implements WebMvcConfigurer{//官方推荐 实现WebMvc
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(demoInterceptor());
 
-	}
+	}	
+	//@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("/index");
+
+}
 	
 }
--- src/main/java/springmvc4/web/HelloController.java
+++ src/main/java/springmvc4/web/HelloController.java
@@ -1,12 +1,12 @@
 package springmvc4.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/index")
-	private String hello() {
-		return "index";
-	}
+//	@RequestMapping("/index")
+//	private String hello() {
+//		return "index";
+//	}
+
 }
