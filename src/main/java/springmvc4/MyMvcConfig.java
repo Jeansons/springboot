a08436ee1bd572c3edaaf63387d869eec2043569
jeansons <494079712@qq.com>
2019-01-22 11:36:45 +0800

    文件上传 MultipartFile setDefaultEncoding("UTF-8")

--- src/main/java/springmvc4/MyMvcConfig.java
+++ src/main/java/springmvc4/MyMvcConfig.java
@@ -3,7 +3,8 @@ package springmvc4;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -16,7 +17,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc  //@EnableWebMvc 开启springmvc支持，若无，重写webmvcConfigurerAdaper方法无效。
-@EnableScheduling
+//@EnableScheduling
 @ComponentScan("springmvc4")
 public class MyMvcConfig implements WebMvcConfigurer{//官方推荐 实现WebMvcConfigurer
 //public class MyMvcConfig extends WebMvcConfigurerAdapter{
@@ -31,6 +32,7 @@ public class MyMvcConfig implements WebMvcConfigurer{//官方推荐 实现WebMvc
 		return viewResolver;
 		
 	}
+
 	public void addResourceHandlers(ResourceHandlerRegistration registry) {
 		registry.addResourceLocations("/assets/**")
 		.addResourceLocations("classpath:/assets/");
@@ -48,11 +50,21 @@ public class MyMvcConfig implements WebMvcConfigurer{//官方推荐 实现WebMvc
 	//@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/index").setViewName("/index");
+		registry.addViewController("/toUpload").setViewName("/upload");
 
 	}
 	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) { //可不忽略“.”
 		configurer.setUseSuffixPatternMatch(false);
+	}
+	
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver=
+				new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("UTF-8"); //编码设置中文要乱码
+		multipartResolver.setMaxUploadSize(5242880);
+		return multipartResolver;
 	}
 	
 }
---
+++ src/main/java/springmvc4/web/ch4_5/UploadController.java
@@ -0,0 +1,33 @@
+package springmvc4.web.ch4_5;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+public class UploadController {
+	
+	@RequestMapping(value = "/upload",method = RequestMethod.POST)
+	public @ResponseBody String upload(MultipartFile file) {//接受上传的文件
+		
+			try {
+				FileUtils.writeByteArrayToFile(
+						new File("e:/upload/"+file.getOriginalFilename()),
+						file.getBytes()); //快速写文件到磁盘
+				return "ok";
+			} catch (IOException e) {
+				e.printStackTrace();
+				return "wrong";
+			}
+			
+		
+	}
+
+}
