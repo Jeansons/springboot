c413f251bbda20c57c2c4f540958a163d06f5f3c
jeansons <494079712@qq.com>
2019-01-23 11:04:05 +0800

    演示

--- src/main/java/springboot/ch5_2_2/ch522Application.java
+++ src/main/java/springboot/ch5_2_2/ch522Application.java
@@ -1,4 +1,5 @@
 package springboot.ch5_2_2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class ch522Application {
+	@Value("${book.author}")
+	private String bookAuthor;
+	@Value("${book.name}")
+	private String bookName;
+	
 	@RequestMapping("/")
 		String index() {
-		return "hello spring boot";
+		return "book name is :"+bookName+" and book author is : "+bookAuthor;
 		}
 	public static void main(String[] args) {
-		SpringApplication.run(ch522Application.class, args);
+		 SpringApplication.run(ch522Application.class,args);
+		
 		
 	}
 
